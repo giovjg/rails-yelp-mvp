c2bcffa4d2b51892a1514e549bc3d7aebed1e92c
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-02-16 16:27:26 +0100

    modified view restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -38,7 +38,7 @@ class RestaurantsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :stars)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
   def find_restaurant
