7a2b38e8114264932e2b130d3f100d8b1f8db393
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-02-16 16:49:43 +0100

    added review new html

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
-  validates :phone_number, presence: true
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
